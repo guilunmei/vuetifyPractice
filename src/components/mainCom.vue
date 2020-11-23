9d12e51bb5731090a067541f57688025c377dddc
guilunmei <7972123+interpret@user.noreply.gitee.com>
2020-11-23 16:43:47 +0800

    23

---
+++ src/components/mainCom.vue
@@ -0,0 +1,5 @@
+<template>
+    <v-row>
+        <h1>测试组件</h1>
+    </v-row>
+</template>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import MainCom from '../components/MainCom.vue'
 
 Vue.use(VueRouter)
 
@@ -8,7 +9,10 @@ const routes = [
   {
     path: '/',
     name: 'Home',
-    component: Home
+    component: Home,
+    children: [
+      { path: '/', component: MainCom }
+    ]
   },
   {
     path: '/about',
